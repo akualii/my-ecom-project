9e2a57f628a789f0481fee177743521db896e2dc
Ashley Kualii <ashkua10@gmail.com>
2020-12-06 03:16:45 -0800

    built layout grid and header navbar components

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://via.placeholder.com/50x50" />
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Header from "./headernavbar/header";
+import Navbar from "./headernavbar/navbar";
 
 class Layout extends Component {
   render() {
     return (
-      <div>
-        <h1>ECOM</h1>
+      <div classNmae="layout">
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
